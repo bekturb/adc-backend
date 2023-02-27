41aff253cbeb1904c3727353de4800d764266455
Bektursun Baimamatov <bektursunbaimamatov@MacBook-Pro-Bektursun.local>
2023-02-27 16:19:18 +0600

    add logging and testing modules

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const express = require('express');
 const app = express();
 const winston = require('winston');
+require("./startup/logging")();
 require('./startup/routes')(app);
 require('./startup/db')();
-
+require('./startup/config')();
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
