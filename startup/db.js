9ea5b33564881ffb7a9f65f9aee853938034d081
Bektursun Baimamatov <bektursunbaimamatov@MacBook-Pro-Bektursun.local>
2023-02-28 23:53:52 +0600

    jwt changed

--- startup/db.js
+++ startup/db.js
@@ -5,9 +5,11 @@ const config = require("config");
 module.exports = function () {
     mongoose.set("strictQuery", true);
     mongoose.connect(config.get("db"), {
-        useUnifiedTopology: true,
-        replicaSet: "rs",
         useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+        replicaSet: "rs",
     })
         .then(() => {
             winston.debug('Successfully connected to mongodb');
