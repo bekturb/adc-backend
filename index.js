ca87beeb9a90bb840b85ad20a756705a05d3796f
Bektursun Baimamatov <bektursunbaimamatov@MacBook-Pro-Bektursun.local>
2023-02-28 14:51:03 +0600

    ended project

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require("./startup/logging")();
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
+require("./startup/prod")(app);
 
 const port = process.env.PORT || 5000;
 const server = app.listen(port, () => {
--- startup/db.js
+++ startup/db.js
@@ -5,9 +5,9 @@ const config = require("config");
 module.exports = function () {
     mongoose.set("strictQuery", true);
     mongoose.connect(config.get("db"), {
+        useUnifiedTopology: true,
         replicaSet: "rs",
         useNewUrlParser: true,
-        useUnifiedTopology: true
     })
         .then(() => {
             winston.debug('Successfully connected to mongodb');
