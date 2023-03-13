1d9e53b3be1b5cb361180a003f0527f93c39f4e6
Bektursun Baimamatov <bektursunbaimamatov@MacBook-Pro-Bektursun.local>
2023-03-13 16:34:54 +0600

    add some mongodb changes

--- startup/db.js
+++ startup/db.js
@@ -4,17 +4,10 @@ const winston = require('winston');
 
 module.exports = function () {
     mongoose.set("strictQuery", true);
-    const connectDb = async () => {
-        try {
-            const conn = await mongoose.connect(process.env.DB_HOST, {
-                useUnifiedTopology: true,
-            })
-                .then(() => {
-                    winston.debug('Successfully connected to mongodb');
-                });
-        } catch (error) {
-            process.exit(1)
-        }
-    }
-
+    mongoose.connect(process.env.DB_HOST, {
+        useUnifiedTopology: true,
+    })
+        .then(() => {
+            winston.debug('Successfully connected to mongodb');
+        });
 }
